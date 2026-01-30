7400821afd3af90ae8deeba04fe720a8cfb64fbb
wjdguswhd <guswhd43@naver.com>
2026-01-30 16:04:17 +0900

    update

--- 05_OOP/src/com/kh/example/chap04_field/controller/KindsOfVariable2.java
+++ 05_OOP/src/com/kh/example/chap04_field/controller/KindsOfVariable2.java
@@ -5,8 +5,10 @@ public class KindsOfVariable2 {
 	//static변수 = 클래스 변수 : static이 붙은 변수
 	public static int staticNum;
 	
+	//인스턴스 변수: static이 붙지않은 변수
 	private int testNum=10;
 	private static int staticTestNum = 10;
+	
 	
 	public void test() {
 		testNum++;
---
+++ 05_OOP/src/com/kh/example/practice1/model/vo/Member.java
@@ -0,0 +1,22 @@
+package com.kh.example.practice1.model.vo;
+
+public class Member {
+	private String memberId;
+	private String memberPwd;
+	private String memberName;
+	private int age;
+	private char gender;
+	private String phone;
+	private String email;
+
+	public void changeName(String name) {
+		memberName = name;
+		
+	}
+	
+	public void printName() {
+		System.out.println(memberName);
+		
+	}
+	
+}
