634f3690fde44fe99de4ba2c1ac363a7ae8aa2ad
wjdguswhd <guswhd43@naver.com>
2026-01-30 14:16:19 +0900

    update

--- 05_OOP/src/com/kh/example/chap04_field/run/Run.java
+++ 05_OOP/src/com/kh/example/chap04_field/run/Run.java
@@ -1,10 +1,17 @@
 package com.kh.example.chap04_field.run;
 
 import com.kh.example.chap04_field.controller.KindsOfVariable1;
+import com.kh.example.chap04_field.controller.KindsOfVariable2;
 
 public class Run {
 	public static void main(String[]args) {
 		KindsOfVariable1 kov1 = new KindsOfVariable1();
 		kov1.method1(100);
+		
+		//KindsOfVaraiable2에 있는 staticNum에 int a1에 대입
+		KindsOfVariable2 kov2 = new KindsOfVariable2();
+		int a1 = kov2.staticNum;
+		int a2 = KindsOfVariable2.staticNum;
+
 	}
 }
