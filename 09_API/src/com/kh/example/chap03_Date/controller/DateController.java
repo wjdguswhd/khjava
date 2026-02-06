133594736a1dffcad0eaf431548fd4fcc09a507e
wjdguswhd <guswhd43@naver.com>
2026-02-06 14:16:53 +0900

    updatee

---
+++ 09_API/src/com/kh/example/chap03_Date/controller/DateController.java
@@ -0,0 +1,13 @@
+package com.kh.example.chap03_Date.controller;
+
+import java.util.Date;
+
+public class DateController {
+	public void method1() {
+		Date today = new Date();
+		System.out.println(today); //Fri Feb 06 14:13:01 KST 2026
+		System.out.println(today); //
+		
+		
+	}
+}
--- 09_API/src/com/kh/example/run/Run.java
+++ 09_API/src/com/kh/example/run/Run.java
@@ -1,11 +1,14 @@
 package com.kh.example.run;
 
-import com.kh.example.chap01_String.controller.StringController;
+import com.kh.example.chap03_Date.controller.DateController;
 
 public class Run {
 	public static void main(String[] args) {
-		StringController sc = new StringController();
-		sc.method3();
+		//StringController sc = new StringController();
+		//sc.method3();
+		
+		DateController dc = new DateController(); 
+		dc.method1();
 		
 	}
 }
