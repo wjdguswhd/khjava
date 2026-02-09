d43ce4d6ffd30b48f3ceaae619fbb4e152853b2c
wjdguswhd <guswhd43@naver.com>
2026-02-09 14:48:50 +0900

    update!!

--- 11_IO/src/com/kh/example/chp00_file/controller/FileController.java
+++ 11_IO/src/com/kh/example/chp00_file/controller/FileController.java
@@ -5,11 +5,33 @@ import java.io.IOException;
 
 public class FileController {
 	public void method() {
-		File f1 = new File("test.txt");
 		try {
+			File f1 = new File("test.txt");
 			f1.createNewFile();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
+			
+			File f2 = new File("c:/test/test.txt");
+			f2.createNewFile();
+			
+			File f3 = new File("c:\\temp1\\temp2");
+			f3.mkdirs();
+			
+			//File f4 = new File("c:\\temp1\\temp2\\test.txt");
+			File f4 = new File(f3, "test.txt");
+			f4.createNewFile();	
+			f4.delete();
+			
+			System.out.println(f2.exists());
+			System.out.println(f3.exists());
+			System.out.println(f4.exists());
+			System.out.println(f3.exists());
+			
+			System.out.println("파일 명 :" + f1.getName());
+			System.out.println("저장절대경로 :" + f1.getAbsolutePath());
+			System.out.println("저장상대경로 :" + f1.getPath());
+			System.out.println("파일용량 :" + f1.length());
+			System.out.println("f4 상위 폴더 :" + f1.getParent());
+			
+		}  catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
---
+++ thispracitce/src/com/kh/practice/leap/run/Run.java
@@ -0,0 +1,5 @@
+package com.kh.practice.leap.run;
+
+public class Run {
+
+}
